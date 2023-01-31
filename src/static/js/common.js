beccaabb086125b5394f6162a849eb305cb8e288
fear220268 <devsequencce@gmail.com>
2023-01-31 09:21:15 +0200

    add css to line

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,9 +1,17 @@
 $('.quiz-item .quiz-item__link button, .quiz-item .quiz-item__link input').on('click', function (e) {
     if($('.quiz-inner .quiz-item:last-of-type').hasClass('active')){}else{
         $('.quiz-step').slideDown();
+
         $('.quiz-step__title .total').html($('.quiz-inner .quiz-item').length - 2);
+
         var myStr = $('.quiz-inner .quiz-item.active').data('quiz').replace(/\D/g,'');
+
         $('.quiz-step__title .current').html(myStr);
+        var ska = $('.quiz-inner .quiz-item').length - 2;
+        var skaBliad = ( Number(myStr) / ska * 100);
+        $('.quiz-step__line span').css('width',  skaBliad + '%').addClass('active');
+
+
         $('.quiz-item.active').removeClass('active').next().addClass('active');
         $('.reviews-item.active').removeClass('active').next().addClass('active');
         $('.quiz-media__item.active').removeClass('active').next().addClass('active');
